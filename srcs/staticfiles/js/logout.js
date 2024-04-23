5d162fd4f2069697f530a4b47cc77676fef76be9
Артем Апенко <lbapart@MacBook-Pro.local>
2024-04-23 21:17:33 +0200

    static files fix and added more stuff to gitignore

--- srcs/staticfiles/js/logout.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Logout handling
-    var logoutLink = document.getElementById('logoutRef');
-    logoutLink.addEventListener('click', function(e) {
-        e.preventDefault();
-        localStorage.removeItem('token');
-        location.reload();
-    });
-});
