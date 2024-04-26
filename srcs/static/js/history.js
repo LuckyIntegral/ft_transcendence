4d448340830d790664a74f6c12ce0f9f7fc3039f
Artem Apenko <aapenko@c2r5p3.42vienna.com>
2024-04-26 15:34:42 +0200

    add browsing history

--- srcs/static/js/history.js
+++ srcs/static/js/history.js
@@ -1,31 +1,34 @@
-// import loadProfile from './profile.js';
+// import loadGamePage from './game.js';
+// import loadProfilePage from './profile.js';
 
-// // Define your routes
-// var routes = {
-//     'profile': function() {
-//         loadProfile();
-//     },
-//     'pong': function() {
-//         document.getElementById('content').textContent = 'Pong page';
-//     },
-//     'default': function() {
-//         document.getElementById('content').textContent = 'Default page';
-//     }
-// };
+// Define your routes
+document.addEventListener('DOMContentLoaded', function() {
+	var routes = {
+		'profile': function() {
+			loadProfilePage();
+		},
+		'pong': function() {
+			loadGamePage();
+		},
+		'default': function() {
+			document.getElementById('content').textContent = 'Default page';
+		}
+	};
 
-// // Function to handle hash changes
-// function handleHashChange() {
-//     var hash = window.location.hash.slice(1);  // Remove the '#' from the hash
-//     var routeFunction = routes[hash];
-//     if (routeFunction) {
-//         routeFunction();
-//     } else {
-//         routes['default']();
-//     }
-// }
+	// Function to handle hash changes
+	function handleHashChange() {
+		var hash = window.location.hash.slice(1);  // Remove the '#' from the hash
+		var routeFunction = routes[hash];
+		if (routeFunction) {
+			routeFunction();
+		} else {
+			routes['default']();
+		}
+	}
 
-// // Listen for hash changes
-// window.addEventListener('hashchange', handleHashChange);
+	// Listen for hash changes
+	window.addEventListener('hashchange', handleHashChange);
 
-// // Handle the initial hash
-// handleHashChange();
+	// Handle the initial hash
+	handleHashChange();
+});
--- srcs/transcedence/settings.py
+++ srcs/transcedence/settings.py
@@ -173,7 +173,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
