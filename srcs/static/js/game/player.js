07311446404c5363696ce16b9ba24aebc81e7154
ARTHUR BIED-CHARRETON <arthurbiedchar@gmail.com>
2024-05-11 22:37:00 +0200

    add: username to Player class

--- srcs/static/js/game/player.js
+++ srcs/static/js/game/player.js
@@ -5,6 +5,27 @@ class Player {
     this.moveDown = false
     this.resetScore()
     this.resetPosition()
+    this.fetchUsername().then(username => {
+      console.log(`Player ${this.username} created`)
+    })
+  }
+
+  fetchUsername () {
+    var url = new URL(`http://${window.location.host}/api/profile/`)
+    return fetchWithToken(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + localStorage.getItem('access')
+      }
+    }).then(response => {
+      if (response.status === 200) {
+        return response.json().then(data => {
+          this.username = data.username
+          return this.username
+        })
+      }
+    })
   }
 
   resetScore () {
