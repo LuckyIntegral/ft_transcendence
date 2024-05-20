a952bd0d44954a93008f847d16c097dda4adf417
ARTHUR BIED-CHARRETON <arthurbiedchar@gmail.com>
2024-05-20 18:30:30 +0200

    test

--- srcs/pingpong/consumers.py
+++ srcs/pingpong/consumers.py
@@ -339,6 +339,9 @@ class GameConsumer(AsyncWebsocketConsumer):
             {"type": "player_connected", "players_connected": len(self.users)},
         )
 
+
+
+
     async def receive(self, text_data):
         data = json.loads(text_data)
         event = data.get("event")
