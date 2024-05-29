78af29e04b40995bebff77429837986f0f2f895d
Roman Perkmann <rperkman@c2r1p2.42vienna.com>
2024-05-29 15:14:52 +0200

    fix: AI also checks for Paddel boundarys
    fix: when kilicking outsite the paddels arent stuck in a weird state
    
    added random ball direction on gamestart

--- srcs/static/js/game/ball.js
+++ srcs/static/js/game/ball.js
@@ -4,8 +4,15 @@ class Ball {
     }
 
     resetPosition() {
-        this.xSpeed = GameConstants.BALL_SPEED;
-        this.ySpeed = GameConstants.BALL_SPEED;
+        const d = new Date();
+        let ms = d.getMilliseconds();
+
+        if (ms > 500) {
+            this.xSpeed = GameConstants.BALL_SPEED;
+        } else {
+            this.xSpeed = -GameConstants.BALL_SPEED;
+        }
+        this.ySpeed = (ms % 10) - 5;
         this.speed = GameConstants.BALL_BASE_ACCELERATION;
         this.x = GameConstants.GAME_WIDTH / 2;
         this.y = GameConstants.GAME_HEIGHT / 2;
